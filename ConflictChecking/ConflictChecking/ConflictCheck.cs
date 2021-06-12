04b1d575144703c4584f1f841bb62e4c7510790a
Anjaneya <anjaneyaprasad9437@gmail.com>
2021-06-12 23:09:47 +0530

    [Anjaneya]Added: checking for conflict

---
+++ ConflictChecking/ConflictChecking/ConflictCheck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConflictChecking
+{
+    public class ConflictCheck
+    {
+        int num1=0, num2 = 1; 
+        int num3;
+       public void FibonacciNumber()
+        {
+            Console.WriteLine("Enter a number here");
+            int count =Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= count; i++)
+            {
+                num3 = num1 + num2;
+                Console.WriteLine(" "+num3);
+                num1 = num2;
+                num2 = num3;
+            }
+
+        }
+    }
+}
---
+++ ConflictChecking/ConflictChecking/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConflictChecking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConflictCheck checking = new ConflictCheck();
+            checking.FibonacciNumber();
+        }
+    }
+}
